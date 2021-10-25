b402de03cdac7c9f41f5b4b65a7d5ce2dce44f29
Carla-Wasovicz <Carla.almeida2@estudante.ifms.edu.br>
2021-10-25 16:54:41 -0400

    Api

---
+++ src/main/java/com/example/ola.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ola {
+    @GetMapping(value="/")
+    public String getMethodName( ) {
+        return "Olá";
+    }
+    
+
+}
